c9cd43f83708fdea88b84cb34e1038036bd4a307
mathieu <mathieu.reboul@epitech.eu>
2020-03-05 11:12:27 +0100

    Article up for API REST

--- ECommerce/src/Controller/ArticleController.php
+++ ECommerce/src/Controller/ArticleController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,6 +24,147 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/api/getAllArticles", name="apiGetAllArticles", methods={"GET"})
+     */
+    public function apiGetAllArticles()
+    {
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
+        $arrayCollection = [];
+
+        /** @var Article $item */
+        foreach($articles as $item) {
+            $arrayCollection[] = array(
+                'id' => $item->getId(),
+                'nom' => $item->getNom(),
+                'categorie' => $item->getCategorie(),
+                'description' => $item->getDescription(),
+                'caracteristiques' => $item->getCaracteristiques(),
+                'prix_unitaire' => $item->getPrixUnitaire(),
+                'poids' => $item->getPoids(),
+                'qte_en_stock' => $item->getQteEnStock(),
+                'is_new' => $item->getIsNew(),
+                'promotion' => $item->getPromotion()
+            );
+        }
+        return new JsonResponse($arrayCollection);
+    }
+
+    /**
+     * @Route("/api/getArticle/{id}", name="apiGetArticle", methods={"GET"})
+     */
+    public function apiGetArticle($id)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $arrayCollection[] = array(
+            'id' => $article->getId(),
+            'nom' => $article->getNom(),
+            'categorie' => $article->getCategorie(),
+            'description' => $article->getDescription(),
+            'caracteristiques' => $article->getCaracteristiques(),
+            'prix_unitaire' => $article->getPrixUnitaire(),
+            'poids' => $article->getPoids(),
+            'qte_en_stock' => $article->getQteEnStock(),
+            'is_new' => $article->getIsNew(),
+            'promotion' => $article->getPromotion()
+        );
+
+        return new JsonResponse($arrayCollection);
+    }
+
+    /**
+     * @Route("/api/createArticle", name="apiCreateArticle", methods={"POST"})
+     */
+    public function apiCreateArticle()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setNom('test');
+        $article->setPoids(0);
+        $article->setPrixUnitaire(0);
+        $article->setDescription('');
+        $article->setCategorie('');
+        $article->setCaracteristiques('');
+        $article->setQteEnStock(0);
+        $article->setIsNew(true);
+        $article->setPromotion(0);
+
+        $arrayCollection[] = array(
+            'id' => $article->getId(),
+            'nom' => $article->getNom(),
+            'categorie' => $article->getCategorie(),
+            'description' => $article->getDescription(),
+            'caracteristiques' => $article->getCaracteristiques(),
+            'prix_unitaire' => $article->getPrixUnitaire(),
+            'poids' => $article->getPoids(),
+            'qte_en_stock' => $article->getQteEnStock(),
+            'is_new' => $article->getIsNew(),
+            'promotion' => $article->getPromotion()
+        );
+
+        $entityManager->persist($article);
+
+        $entityManager->flush();
+
+        return new JsonResponse($arrayCollection);
+    }
+
+    /**
+     * @Route("/api/manageArticle/{id}", name="apiManageArticle", methods={"POST"})
+     */
+    public function apiManageArticle($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $article->setNom('modifnom');
+        $article->setCategorie('modif');
+        $article->setDescription('modif');
+        $article->setCaracteristiques('modif');
+        $article->setPrixUnitaire(60);
+        $article->setPoids(9.2);
+        $article->setQteEnStock(10);
+        $article->setIsNew(true);
+        $article->setPromotion(10);
+
+        $arrayCollection[] = array(
+            'id' => $article->getId(),
+            'nom' => $article->getNom(),
+            'categorie' => $article->getCategorie(),
+            'description' => $article->getDescription(),
+            'caracteristiques' => $article->getCaracteristiques(),
+            'prix_unitaire' => $article->getPrixUnitaire(),
+            'poids' => $article->getPoids(),
+            'qte_en_stock' => $article->getQteEnStock(),
+            'is_new' => $article->getIsNew(),
+            'promotion' => $article->getPromotion()
+        );
+
+        $entityManager->persist($article);
+
+        $entityManager->flush();
+
+        return new JsonResponse($arrayCollection);
+    }
+
+    /**
+     * @Route("/api/deleteArticle/{id}", name="apiDeleteArticle", methods={"POST"})
+     */
+    public function apiDeleteArticle($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        $entityManager->remove($article);
+
+        $entityManager->flush();
+
+        return new JsonResponse(array("deletion" => "success"));
     }
 
     /**
